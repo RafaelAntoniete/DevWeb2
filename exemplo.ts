1e9226339ab0fca11543e1b15ebfe204b1099246
RafaelAntoniete <rafaelantoniete0@gmail.com>
2023-08-11 20:21:24 -0300

    11/08/2023

---
+++ exemplo.ts
@@ -0,0 +1,17 @@
+namespace exemplo1 {
+    let n1: number;
+    n1 = 10;
+
+    let resultado : number;
+    resultado = n1 + n1;
+    console.log("A soma entre " + n1 +" com " + n1 + " é igual a: " + resultado);
+
+    resultado = n1 - 4;
+    console.log(`A subtração entre ${n1} com 4 é igual a: ${resultado}`);
+
+    resultado = n1 * 2;
+    console.log (`O dobro de ${n1} é: ${resultado}!!`);
+
+    resultado = n1 / 2;
+    console.log (`A metade de ${n1} é: ${resultado}!!`);
+}
---
+++ exercicio01.ts
@@ -0,0 +1,11 @@
+namespace exercicio01 {
+    let n1, n2, n3, n4: number;
+    n1 = 56;
+    n2 = 12;
+    n3 = 29;
+    n4 = 46;
+
+    let result: number;
+    result = n1 + n2 + n3 + n4;
+    console.log (`O resultado da soma`)
+}
