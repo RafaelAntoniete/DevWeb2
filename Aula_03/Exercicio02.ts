6446ee27d5d03a860f9d5e2212bafd9c9af02094
RafaelAntoniete <rafaelantoniete0@gmail.com>
2023-08-28 20:17:53 -0300

    28/08/2023

---
+++ Aula_03/Exercicio02.ts
@@ -0,0 +1,14 @@
+namespace exercicio2 {
+    let lista: number[] = [2, 7, -1, 3, 40, 12];
+    let maior: number = lista[0]
+
+    for(let i = 1; i <= lista.length; i++){
+        if(maior < lista[i]){
+            maior = lista[i];
+        }
+    }
+
+    console.log(maior)
+
+    console.log(`O maior valor é: ${Math.max(... lista)}`);
+}
